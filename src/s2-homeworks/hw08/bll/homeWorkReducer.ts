689732ba178b512467ec1d09aa77843f305392e4
Dmitriy <goglev37@gmail.com>
2024-08-04 22:36:21 +0300

    задание №8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,19 +1,27 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8"
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+    | { type: "sort"; payload: "up" | "down" }
+    | { type: "check"; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case "sort": { // by name
+            if (action.payload === "up") {
+                const newState = [...state]
+                return newState.sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if (action.payload === "down") {
+                const newState = [...state]
+                return newState.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return state
+            // need to fix
         }
-        case 'check': {
-
-            return state // need to fix
+        case "check": {
+            return state.filter(e => e.age >= action.payload) // need to fix
         }
+
         default:
             return state
     }
