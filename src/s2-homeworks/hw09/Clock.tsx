e29dec55175649bf4269f64e49636b96a99c1d15
Dmitriy <goglev37@gmail.com>
2024-08-11 00:26:25 +0300

    задание №9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -37,7 +37,7 @@ function Clock() {
 
     const stringTime = `${get2digitString(date.getHours())}:${get2digitString(date.getMinutes())}:${get2digitString(date.getSeconds())} ` ||
         <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${get2digitString(date.getDay())}:${get2digitString(date.getMonth())}:${get2digitString(date.getFullYear())} ` ||
+    const stringDate = `${get2digitString(date.getDay())}.${get2digitString(date.getMonth())}.${get2digitString(date.getFullYear())} ` ||
         <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
