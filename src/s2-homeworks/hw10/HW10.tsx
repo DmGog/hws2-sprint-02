98c18d1ac47676a0ce51a4c1a8388c5e6f117013
Dmitriy <goglev37@gmail.com>
2024-08-16 15:47:50 +0300

    задание №10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import s2 from '../../s1-main/App.module.css'
-import {Loader} from './Loader'
+import React from "react"
+import {useDispatch, useSelector} from "react-redux"
+import {AppStoreType} from "./bll/store"
+import {loadingAC} from "./bll/loadingReducer"
+import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
+import s2 from "../../s1-main/App.module.css"
+import {Loader} from "./Loader"
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -15,26 +15,29 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+    const setLoading = () => {
+        dispatch(loadingAC(true))
+        const timerId = setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
+        return () => clearTimeout(timerId)
     }
 
     return (
-        <div id={'hw10'}>
+        <div id={"hw10"}>
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
                 {isLoading ? (
-                    <div id={'hw10-loading'}>
+                    <div id={"hw10-loading"}>
                         <Loader/>
                     </div>
                 ) : (
                     <SuperButton
-                        id={'hw10-button-start-loading'}
+                        id={"hw10-button-start-loading"}
                         onClick={setLoading}
                     >
                         Set loading...
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,27 @@
-const initState = {
+const initState: stateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+export type stateType = {
+    isLoading: boolean
+}
 
+export const loadingReducer = (state = initState, action: ActionsType): stateType => { // fix any
+    switch (action.type) {
+        case "CHANGE_LOADING":
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
+    type: "CHANGE_LOADING"
     isLoading: boolean
 }
+type ActionsType = LoadingActionType
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
+    type: "CHANGE_LOADING",
     isLoading,
 })
