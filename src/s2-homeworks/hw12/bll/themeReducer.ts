267397de1207a873bd4199f715e59c8c631f186f
Dmitriy <goglev37@gmail.com>
2024-08-30 18:24:00 +0300

    задание №12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
 const initState = {
     themeId: 1,
 }
+type InitState = typeof initState
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionsType): InitState => { // fix any
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID": {
+            return {...state, themeId: action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionsType = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({type: "SET_THEME_ID", id} as const) // fix any
